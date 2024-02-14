2ee122c88df69c6db482a9cf8e1c172b7fa11f66
GVignesh-SD <gvignesh380980@gmail.com>
2024-02-15 00:35:57 +0530

    added loading effects

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -4,6 +4,8 @@ import React from "react";
 
 const ImageSearchPage = async ({ searchParams }) => {
   const startIndex = searchParams.start || "1";
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&searchType=image&start=${startIndex}`
   );
