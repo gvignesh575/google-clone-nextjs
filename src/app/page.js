d9262e466d64fae44b3bb9ca925c4f43e8d2cdb7
GVignesh-SD <gvignesh380980@gmail.com>
2024-02-14 18:45:54 +0530

    Added the Header Component in Home Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
+import HomeHeader from "@/components/HomeHeader";
 import React from "react";
 
 const HomePage = () => {
-  return <div className="text-red-500">Google Clone!</div>;
+  return (
+    <>
+      <HomeHeader />
+    </>
+  );
 };
 
 export default HomePage;
