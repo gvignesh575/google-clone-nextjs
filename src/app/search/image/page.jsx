6013b955efa70b6aec51f8272a47688402ec4d68
GVignesh-SD <gvignesh380980@gmail.com>
2024-02-15 00:35:57 +0530

    completed

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,15 +1,13 @@
 import ImageSearchResults from "@/components/ImageSearchResults";
 import Link from "next/link";
-import React from "react";
 
-const ImageSearchPage = async ({ searchParams }) => {
+export default async function ImageSearchPage({ searchParams }) {
   const startIndex = searchParams.start || "1";
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&searchType=image&start=${startIndex}`
   );
-  if (!response.ok) {
-    throw new Error("Something went wrong");
-  }
+  if (!response.ok) throw new Error("Something went wrong");
   const data = await response.json();
   const results = data.items;
 
@@ -29,15 +27,5 @@ const ImageSearchPage = async ({ searchParams }) => {
     );
   }
 
-  return (
-    <div>
-      {results && (
-        <>
-          <ImageSearchResults results={data} />
-        </>
-      )}
-    </div>
-  );
-};
-
-export default ImageSearchPage;
+  return <div>{results && <ImageSearchResults results={data} />}</div>;
+}
--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,15 +1,13 @@
 import WebSearchResults from "@/components/WebSearchResults";
 import Link from "next/link";
-import React from "react";
 
-const WebSearchPage = async ({ searchParams }) => {
+export default async function WebSearchPage({ searchParams }) {
   const startIndex = searchParams.start || "1";
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&start=${startIndex}`
   );
-  if (!response.ok) {
-    throw new Error("Something went wrong");
-  }
+  if (!response.ok) throw new Error("Something went wrong");
   const data = await response.json();
   const results = data.items;
 
@@ -29,15 +27,5 @@ const WebSearchPage = async ({ searchParams }) => {
     );
   }
 
-  return (
-    <div>
-      {results && (
-        <>
-          <WebSearchResults results={data} />
-        </>
-      )}
-    </div>
-  );
-};
-
-export default WebSearchPage;
+  return <div>{results && <WebSearchResults results={data} />}</div>;
+}
