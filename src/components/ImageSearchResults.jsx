bc91c6b357904f0abeb77b28d672e0fb685898ea
GVignesh-SD <gvignesh380980@gmail.com>
2024-02-15 00:20:07 +0530

    implemented the pagination functionality

--- src/components/ImageSearchResults.jsx
+++ src/components/ImageSearchResults.jsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import React from "react";
+import PaginationButtons from "./PaginationButtons";
 
 const ImageSearchResults = ({ results }) => {
   return (
-    <div className="pb-24 mt-4">
+    <div className="sm:pb-24 pb-40 mt-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4">
         {results.items.map((result) => (
           <div className="mb-8" key={result.link}>
@@ -28,6 +29,9 @@ const ImageSearchResults = ({ results }) => {
             </div>
           </div>
         ))}
+      </div>
+      <div className="ml-16">
+        <PaginationButtons />
       </div>
     </div>
   );
