ae73c3df77b6fb743955d14e4c88e776170fa16b
GVignesh-SD <gvignesh380980@gmail.com>
2024-02-20 22:07:19 +0530

    fixed the responsive

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -11,14 +11,13 @@ const SearchHeader = () => {
     <header className="sticky top-0 bg-white">
       <div className="flex w-full p-6 items-center justify-between">
         <Link href={"/"}>
-          <Image
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/375px-Google_2015_logo.svg.png"
-            alt={"Google Logo"}
-            width={120}
-            height={40}
-            priority
-            style={{ width: "auto" }}
-          />
+          <div className="relative w-[60px] h-[40px] border">
+            <Image
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/375px-Google_2015_logo.svg.png"
+              alt={"Google Logo"}
+              fill
+            />
+          </div>
         </Link>
         <div className="flex-1">
           <SearchBox />
@@ -27,7 +26,7 @@ const SearchHeader = () => {
           <RiSettings3Line className="bg-transparent hover:bg-gray-200 p-2 text-4xl rounded-full cursor-pointer" />
           <TbGridDots className="bg-transparent hover:bg-gray-200 p-2 text-4xl rounded-full cursor-pointer" />
         </div>
-        <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow ml-2">
+        <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow ml-2 whitespace-nowrap">
           Sign in
         </button>
       </div>
