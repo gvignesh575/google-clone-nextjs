5e5dea83bffb2102e47203025749413560f2a399
GVignesh-SD <gvignesh380980@gmail.com>
2024-02-20 23:28:00 +0530

    fixed the header responsiveness

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -8,10 +8,10 @@ import SearchHeaderOptions from "./SearchHeaderOptions";
 
 const SearchHeader = () => {
   return (
-    <header className="sticky top-0 bg-white">
+    <header className="sticky top-0 bg-white w-full">
       <div className="flex w-full p-6 items-center justify-between">
         <Link href={"/"}>
-          <div className="flex justify-center items-center relative w-[60px] h-[40px] md:w-[120px] md:h-[60px]">
+          <div className="flex justify-center items-center relative w-[80px] h-[40px] md:w-[120px] md:h-[60px] mt-3">
             <Image
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/375px-Google_2015_logo.svg.png"
               alt={"Google Logo"}
@@ -26,9 +26,6 @@ const SearchHeader = () => {
           <RiSettings3Line className="bg-transparent hover:bg-gray-200 p-2 text-4xl rounded-full cursor-pointer" />
           <TbGridDots className="bg-transparent hover:bg-gray-200 p-2 text-4xl rounded-full cursor-pointer" />
         </div>
-        <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow ml-2 whitespace-nowrap">
-          Sign in
-        </button>
       </div>
       <SearchHeaderOptions />
     </header>
